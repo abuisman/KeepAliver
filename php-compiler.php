c0986348cd49c483c7bed07aab9d1084f2e9c280
Achilleas Buisman <accounts@abuisman.nl>
2011-10-19 17:41:19 +0200

    Added the php compiler script to compile normal javascript to a bookmarklet.

---
+++ php-compiler.php
@@ -0,0 +1,13 @@
+<?php
+$bookmarklet = <<<JS
+var s=document.createElement('script');
+    s.setAttribute('src','http://jquery.com/src/jquery-latest.js');
+    document.getElementsByTagName('body')[0].appendChild(s);
+    var s2=document.createElement('script');
+    s2.setAttribute('type','text/javascript');
+    s2.text = "var ti=function(){jQuery.ajax({url:''});setTimeout('ti()', 36000);};ti();";
+    document.getElementsByTagName('body')[0].appendChild(s2);
+JS;
+
+$bookmarklet = "javascript:".str_replace("\n", '', $bookmarklet);
+print str_replace(' ', '%20', $bookmarklet);

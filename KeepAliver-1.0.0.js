3ed582cb2a79dd0f18a9f8d6bbbc7f204399ac5f
Achilleas Buisman <accounts@abuisman.nl>
2011-10-19 17:40:26 +0200

    Added the bookmarklet code, both compiled and full version.

---
+++ KeepAliver-1.0.0.js
@@ -0,0 +1,7 @@
+var s=document.createElement('script');
+    s.setAttribute('src','http://jquery.com/src/jquery-latest.js');
+    document.getElementsByTagName('body')[0].appendChild(s);
+    var s2=document.createElement('script');
+    s2.setAttribute('type','text/javascript');
+    s2.text = "var ti=function(){jQuery.ajax({url:''});setTimeout('ti()', 36000);};ti();";
+    document.getElementsByTagName('body')[0].appendChild(s2);
